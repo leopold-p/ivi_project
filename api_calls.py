64837f27ea3a1a6d38b088c6600b3d48f49faa5b
Leopold <pashtet123@gmail.com>
2019-07-01 01:11:50 +0300

    add working test script and requirements.txt

--- api_calls.py
+++ api_calls.py
@@ -1,8 +1,41 @@
 import requests
+from urllib.parse import quote
+
 
 class API:
-    def __init__(self, user, password):
-        self.user = user
-        self.password = password
+    def __init__(self, address, user, password):
+        self.address = address
+        self.session = requests.session()
+        self.session.auth = (user, password)
+
+    def __request(self, method, url, data=None):
+        return self.session.request(method, self.address+quote(url), json=data)
+
+    def get_all_characters(self):
+        # GET http://rest.test.ivi.ru/characters
+        return self.__request('GET', 'characters')
+
+    def get_character(self, name):
+        # GET http://rest.test.ivi.ru/character/{name}
+        return self.__request('GET', 'character/{}'.format(name))
+
+    def insert_character(self, data):
+        # POST http://rest.test.ivi.ru/character
+        return self.__request('POST', 'character', data)
+
+    def modify_character(self, name, data):
+        # PUT http://rest.test.ivi.ru/character/{name}
+        return self.__request('PUT', 'character/{}'.format(name), data)
+
+    def delete_character(self, name):
+        # DELETE http://rest.test.ivi.ru/character/{name}
+        return self.__request('DELETE', 'character/{}'.format(name))
+
+    def reset_collection(self):
+        # POST http://rest.test.ivi.ru/reset
+        return self.__request('POST', 'reset')
 
 
+if __name__ == '__main__':
+    api = API('http://rest.test.ivi.ru/', 'pashtet123@gmail.com', 'hgJH768Cv23')
+    api.reset_collection()
---
+++ test_api.py
@@ -0,0 +1,119 @@
+import pytest
+import json
+import random
+from api_calls import API
+
+
+def character_template(name='dude', pop_key=None):
+    template = {
+        "education": "",
+        "height": 0,
+        "identity": "",
+        "name": name,
+        "other_aliases": "",
+        "universe": "",
+        "weight": 0
+    }
+    if pop_key:
+        template.pop(pop_key)
+    return template
+
+
+@pytest.fixture(scope='session')
+def api():
+    a = API('http://rest.test.ivi.ru/', 'pashtet123@gmail.com', 'hgJH768Cv23')
+    yield a
+    a.reset_collection()
+
+
+@pytest.fixture()
+def bad_auth_api():
+    return API('http://rest.test.ivi.ru/', 'pashtet123@gmail.com', '')
+
+
+def test_wrong_auth(bad_auth_api):
+    r = bad_auth_api.get_all_characters()
+    assert r.status_code == 401
+
+
+@pytest.mark.dependency()
+def test_get_characters(api):
+    request = api.get_all_characters()
+    assert request.status_code == 200
+    api.characters = json.loads(request.text)["result"]
+
+
+@pytest.mark.dependency(depends=["test_get_characters"])
+def test_bd_content(api, expected_length=1):
+    assert len(api.characters) >= expected_length
+
+
+@pytest.mark.dependency(depends=["test_bd_content"])
+def test_get_user(api):
+    character = random.choice(api.characters)
+    get_character = api.get_character(character["name"])
+    result = json.loads(get_character.text)["result"][0]
+    assert result == character
+
+
+@pytest.mark.parametrize('name', ['Good_Name', '11111', 'G', '10g'])
+def test_insert_character(api, name):
+    insert = api.insert_character(character_template(name))
+    get_character = api.get_character(name)
+    assert json.loads(insert.text)["result"] == json.loads(get_character.text)["result"][0]
+
+
+@pytest.mark.parametrize('pop_key', character_template().keys())
+def test_insert_broken_character(api, pop_key, name="failanyway"):
+    insert = api.insert_character(character_template(name, pop_key))
+    assert insert.status_code == 500  # TODO: ensure that 500 is correct here and request should fail
+    get_character = api.get_character(name)
+    assert json.loads(get_character.text) == {"result": "No such name"}
+
+
+@pytest.mark.dependency(depends=["test_bd_content"])
+@pytest.mark.parametrize('change_key', character_template(pop_key="name").keys())
+def test_modify_character(api, change_key):
+    data = random.choice(api.characters)
+    name = data["name"]
+    data[change_key] = "i am changed"
+    modify_result = api.modify_character(name, data)
+    assert modify_result.status_code == 200
+    get_character = api.get_character(name)
+    assert json.loads(get_character.text)["result"][0] == data
+
+
+@pytest.mark.dependency(depends=["test_bd_content"])
+@pytest.mark.parametrize('pop_key', character_template().keys())
+def test_invalid_modify_character(api, pop_key):
+    data = random.choice(api.characters)
+    name = data["name"]
+    data.pop(pop_key)
+    modify_result = api.modify_character(name, data)
+    assert modify_result.status_code == 500
+
+
+@pytest.mark.dependency(depends=["test_bd_content"])
+def test_delete_existing_character(api):
+    name = random.choice(api.characters)["name"]
+    delete_result = api.delete_character(name)
+    get_character = api.get_character(name)
+    assert "is deleted" in delete_result.text and "No such name" in get_character.text
+
+
+def test_delete_nonexistent_character(api):
+    name = "i_do_not_exist"
+    delete_result = api.delete_character(name)
+    assert "No such name" in delete_result.text
+
+
+@pytest.mark.dependency(depends=["test_bd_content"])
+def test_db_limit(api, db_limit=500):
+    characters_needed = db_limit - len(api.characters)
+    for i in range(characters_needed):
+        api.insert_character(character_template("dummy" + str(i)))
+    r = api.insert_character(character_template("failing"))
+    assert r.status_code == 400 and json.loads(r.text) == {"error": "Collection can't contain more than 500 items"}
+
+
+
